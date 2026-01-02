c4ad694d854e6b2d3447bf4422aad9c428ab7390
Jacob Weisz <inbox@jacobweisz.com>
2026-01-02 01:53:19 -0600

    Add CCS811 gas sensor

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Nanoprobes are ESP-based home automation sensing units. The idea here is to create simple platforms that can be distributed around a house for a main server to collect data from. In the interests of being standards-based as possible, the goal is to respond over HTTP with a simple plaintext format that can be easily parsed by any language.
 
-The initial test nanoprobe currently programmed here is a ESP8266-powered Feather HUZZAH with a DHT11 sensor assigned to pin 16 for testing.
+The initial test nanoprobe currently programmed here is a ESP8266-powered Feather HUZZAH with a DHT11 sensor assigned to pin 16 and a CCS811 attached to the I2C bus.
 
 Most of this project is based on tutorials found at https://randomnerdtutorials.com
+
+CCS811.py was grabbed from https://github.com/stigtsp/CCS811-micropython
+
+I used uPyCraft V1.1 to flash the device.
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import usocket, socket, dht, machine
-from machine import Pin
+import usocket, socket, dht, machine, CCS811
+from machine import Pin, I2C
 
 envsensor = dht.DHT11(Pin(16))
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+gassensor = CCS811.CCS811(i2c)
 
-def web_page(temp, humid):
-  html = """NanoAPI 1 TemperatureC """ + str(temp) + """ HumidityR% """ + str(humid)
+def web_page(temp, humid, eco2, tvoc):
+  html = """NanoAPI 1 TemperatureC """ + str(temp) + """ HumidityR% """ + str(humid) + """ eCO2ppm """ + str(eco2) + """ tVOCppb """ + str(tvoc)
   return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +24,9 @@ while True:
   envsensor.measure()
   temp = envsensor.temperature()
   humid = envsensor.humidity()
-  response = web_page(temp, humid)
+  eco2 = gassensor.eCO2
+  tvoc = gassensor.tVOC
+  response = web_page(temp, humid, eco2, tvoc)
   conn.send('HTTP/1.1 200 OK\r\n')
   conn.send('Content-Type: text/html\r\n')
   conn.send('Connection: close\r\n\r\n')
