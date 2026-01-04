ff65e0d2a45af27fb9520051931493610a32b59c
Jacob Weisz <inbox@jacobweisz.com>
2026-01-03 20:06:15 -0600

    Fix CCS811 debug, add PIR sensor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Nanoprobes are ESP-based home automation sensing units. The idea here is to create simple platforms that can be distributed around a house for a main server to collect data from. In the interests of being standards-based as possible, the goal is to respond over HTTP with a simple plaintext format that can be easily parsed by any language.
 
-The initial test nanoprobe currently programmed here is a ESP8266-powered Feather HUZZAH with a DHT11 sensor assigned to pin 16 and a CCS811 attached to the I2C bus.
+The initial test nanoprobe currently programmed here is a ESP8266-powered Feather HUZZAH with a DHT11 sensor assigned to pin 16, a PIR sensor assigned to pin 13, and a CCS811 attached to the I2C bus.
 
 Most of this project is based on tutorials found at https://randomnerdtutorials.com
 
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from ccs811factory import CCS811Factory
 
 envsensor = dht.DHT11(Pin(16))
 gassensor = CCS811Factory(I2C(scl=Pin(5), sda=Pin(4)))
+pirsensor = Pin(13, Pin.IN)
 
-def web_page(temp, humid, eco2, tvoc):
-  html = """NanoAPI 1 TemperatureC """ + str(temp) + """ HumidityR% """ + str(humid) + """ eCO2ppm """ + str(eco2) + """ TVOCppb """ + str(tvoc)
+def web_page(temp, humid, eco2, tvoc, motion):
+  html = """NanoAPI 1 TemperatureC """ + str(temp) + """ HumidityR% """ + str(humid) + """ eCO2ppm """ + str(eco2) + """ TVOCppb """ + str(tvoc) + """ Motion """ + str(motion)
   return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,8 +29,9 @@ while True:
   gassensor.humidity = humid
   eco2 = gassensor.eco2
   tvoc = gassensor.etvoc
-  print(ccs811.eco2, "ppm and ", ccs811.etvoc, "ppb")
-  response = web_page(temp, humid, eco2, tvoc)
+  motion = pirsensor.value()
+  print(gassensor.eco2, "ppm and ", gassensor.etvoc, "ppb")
+  response = web_page(temp, humid, eco2, tvoc, motion)
   conn.send('HTTP/1.1 200 OK\r\n')
   conn.send('Content-Type: text/html\r\n')
   conn.send('Connection: close\r\n\r\n')
